408de78e60eb425d71f3cdd6b759561e6fcdf45b
Koumare <ibrahimkoumare5@gmail.com>
2024-09-11 16:39:38 +0100

    Initial commit

---
+++ src/main/java/com/example/api_rest_forum/ApiRestForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_rest_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/api_rest_forum/ApiRestForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rest_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
