79b00d298cd2a08ce788ddaf05655de0a71ae3b5
Koumare <ibrahimkoumare5@gmail.com>
2024-09-13 08:47:36 +0100

    Mise a jour du fichier application.prooerty

---
+++ src/main/java/com/example/api_rest_forum/web/ressource/ForumRessource.java
@@ -0,0 +1,11 @@
+package com.example.api_rest_forum.web.ressource;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/forums")
+public class ForumRessource {
+}
